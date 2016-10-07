0def7c55906188be0032aceeed77c1c265c52038
Nicholas <krylik@rigaming.co.za>
2016-10-07 20:33:57 +0200

    Test new name with dep links

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from distutils.core import setup
 
 setup(
-    name='dict2xml',
-    version='2.0.1',
+    name='dict2xml2',
+    version='0.0.1',
     description='JSON-like dictionary to XML converter',
     author=u'Lucas Inojosa',
     author_email='lucas.inojosa@gmail.com',
